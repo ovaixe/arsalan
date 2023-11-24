3babe77686f4341339451b3f7a9716b0d32517c8
Bhat Owais <owaisbhat996@outlook.com>
2023-11-24 19:01:51 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./components/user/Navbar";
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import AdminDashboard from "./components/user/AdminDashboard";
+import UserDashboard from "./components/user/UserDashboard";
 
 function App() {
+  const [currentComponent, setCurrentComponent] = useState(null);
+
+  const handleLogin = () => {
+    setCurrentComponent("Login");
+  };
+
+  const handleSignUp = () => {
+    setCurrentComponent("SignUp");
+  };
+
+  const handleAdminDashboard = () => {
+    setCurrentComponent("AdminDashboard");
+  };
+
+  const handleUserDashboard = () => {
+    setCurrentComponent("UserDashboard");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar
+        handleLogin={handleLogin}
+        handleSignUp={handleSignUp}
+        handleAdminDashboard={handleAdminDashboard}
+        handleUserDashboard={handleUserDashboard}
+        currentComponent={currentComponent}
+      />
+      <div className="px-20 py-10">
+        {currentComponent === "Login" ? <Login /> : <></>}
+        {currentComponent === "SignUp" ? <SignUp /> : <></>}
+        {currentComponent ==='AdminDashboard' ? <AdminDashboard /> : <></>}
+        {currentComponent ==='UserDashboard' ? <UserDashboard /> : <></>}
+      </div>
     </div>
   );
 }
